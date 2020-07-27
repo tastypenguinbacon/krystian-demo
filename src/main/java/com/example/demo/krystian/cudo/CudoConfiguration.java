af6728c2d80711b951024e526763b0d9bf61b00b
Adrian Jałoszewski <jaloszewski.adrian@gmail.com>
2020-07-27 20:56:21 +0200

    Add files via upload
    Some small changes

--- src/main/java/com/example/demo/krystian/cudo/CudoConfiguration.java
+++ src/main/java/com/example/demo/krystian/cudo/CudoConfiguration.java
@@ -3,11 +3,16 @@ package com.example.demo.krystian.cudo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Configuration
+@Component
 public class CudoConfiguration {
+    private int prototypeCount;
+
     @Bean
     @Qualifier("LiczboweCudo")
     public Cudo cudo3() {
@@ -24,5 +29,12 @@ public class CudoConfiguration {
     public Cudo randomoweCudo() {
         Random r = new Random();
         return () -> System.out.println("Random Cudo: " + r.nextInt(10));
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Cudo prototypoweCudo() {
+        int forClosure = prototypeCount++;
+        return () -> System.out.println("I'm a prototype number: " + forClosure);
     }
 }
---
+++ src/main/java/com/example/demo/krystian/cudo/PorąbaneCudo.java
@@ -0,0 +1,29 @@
+package com.example.demo.krystian.cudo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PorąbaneCudo implements Cudo {
+    @Autowired
+    private CudoConfiguration cudoConfiguration;
+
+    @Override
+    public void doCudo() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\tPotrącony jestem");
+            System.out.print("\t");
+            Cudo c = cudoConfiguration.prototypoweCudo();
+            System.out.print(c + "\t");
+            c.doCudo();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\tPotrącony jestem");
+            System.out.print("\t");
+            Cudo c = cudoConfiguration.cudo3();
+            System.out.print(c + "\t");
+            c.doCudo();
+        }
+    }
+}

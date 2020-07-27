4999ad5c8995174beea108cd247a7c54941145cc
Adrian Jałoszewski <jaloszewski.adrian@gmail.com>
2020-07-27 22:11:12 +0200

    Update CudoConfiguration.java

--- src/main/java/com/example/demo/krystian/cudo/CudoConfiguration.java
+++ src/main/java/com/example/demo/krystian/cudo/CudoConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
